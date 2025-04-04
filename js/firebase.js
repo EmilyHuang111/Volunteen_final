c11d89dd68e9a46b2e0291afb9ee04a69ef619e3
Emily Huang <144239497+EmilyHuang111@users.noreply.github.com>
2025-04-04 18:50:07 -0400

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,5 @@
+/***** 1. Initialize Firebase *****/
+  const app = firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const database = firebase.database();
+  const storage = firebase.storage();
